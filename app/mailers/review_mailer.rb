43c92f44727a05b1322f8cebc093344391dd90b0
Tim Carlock <timothy.carlock@gmail.com>
2011-10-01 15:48:59 -0700

    Mail changes

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -10,6 +10,7 @@ class ReviewMailer < ActionMailer::Base
   def new_request_alert request
     @business = request.business
     @sender = request.user.profile
+    @request = request
 
     mail(:to => request.email, :subject => "#{@sender.first_name} #{@sender.last_name} at #{request.business.name} has requested that you submit a review...")
   end
