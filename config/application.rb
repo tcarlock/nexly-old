e443c495278a59a1d6dc71733532af14df2106b4
Tim Carlock <timothy.carlock@gmail.com>
2011-10-01 15:55:10 -0700

    Mail changes

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Nexly
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.default_url_options = { :host => "nexly.herokuapp.com" }
   end
 end
