e64c4bbcdde631c0ae8b612528c01b04f60d43aa
Tim Carlock <timothy.carlock@gmail.com>
2011-10-01 21:30:19 -0700

    Mail changes

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,20 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Nexly::Application.initialize! do |config|
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
+
+  # Initialize mailer and SMTP
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 end
+
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
 
 Time::DATE_FORMATS.merge!(
   :default => "%b %d at %I:%M%p",
