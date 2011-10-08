07a760a41e22a11ad539b4fa862cfba214a198e1
Tim Carlock <timothy.carlock@gmail.com>
2011-10-08 03:13:50 -0700

    Misc changes

--- app/mailers/recommendation_mailer.rb
+++ app/mailers/recommendation_mailer.rb
@@ -2,6 +2,6 @@ class RecommendationMailer < ActionMailer::Base
   def new_recommendation recommendation
     @rec = recommendation
     
-    mail(:from: "admin@nexly.com", :to => recommendation.email, :subject => "#{recommendation.name} has a recommendation for you...")
+    mail(:from => "admin@nexly.com", :to => recommendation.email, :subject => "#{recommendation.name} has a recommendation for you...")
   end
 end
