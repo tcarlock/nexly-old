0f544a5f8fe76a8b492606648470d6967abdc420
Tim Carlock <timothy.carlock@gmail.com>
2011-10-17 15:39:48 -0700

    Changes for review

--- db/seeds.rb
+++ db/seeds.rb
@@ -155,12 +155,14 @@ end
 Application.create!(
                 :title => 'Reviews',
                 :details => 'Reviews allows you to easily request and manage client reviews.  It also allows you to distribute those reviews to your various social media pages and your own website',
-                :price => 24.99)
+                :price => 24.99,
+                :is_public => true)
 
 Application.create!(
                 :title => 'Resources',
                 :details => 'Resources allows you to easily publish and manage learning materials and information such as articles, white papers, links, etc.',
-                :price => 34.99)
+                :price => 34.99,
+                :is_public => false)
                 
 Platform.create!(:name => "twitter")
 Platform.create!(:name => "facebook")
