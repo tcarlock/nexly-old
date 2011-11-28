b8d0ba1b2c8643d67127589efcaf3de3227a837d
Tim Carlock <timothy.carlock@gmail.com>
2011-11-28 16:31:30 -0600

    AWS config changes

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,6 +2,6 @@ if Rails.env == "production"
    S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "nexly_avatar_prod"} 
  elsif Rails.env == "development"
    S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "nexly_avatar_dev"} 
- elsif Rails.env == "test"
+ elsif Rails.env == "staging"
    S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "nexly_avatar_test"} 
 end
