e1ffeab024eededc1d7e02aef4326f8fb4129bce
Tim Carlock <timothy.carlock@gmail.com>
2011-12-08 15:59:57 -0800

    Requests bug fix

--- app/controllers/review_requests_controller.rb
+++ app/controllers/review_requests_controller.rb
@@ -13,15 +13,15 @@ class ReviewRequestsController < ApplicationController
     
     params[:emails].split(",").each do |email|
       review_request = business.review_requests.create(
-                 :email => email,
+                 :email => email.strip,
                  :message => params[:message],
                  :business_id => params[:business_id],
                  :user_id => current_user.id)
                  
-      ReviewMailer.new_request_alert(review_request).deliver if review_request.valid?
+      #ReviewMailer.new_request_alert(review_request).deliver if review_request.valid?
     end
     
-    redirect_to(business, :notice => "Your requests have been sent")
+    redirect_to(business_review_requests_path(@business), :notice => "Your requests have been sent")
   end
   
   def destroy
