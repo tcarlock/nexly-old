51750e2c7e4b7c011a56b34929b665298dbf3847
Tim Carlock <timothy.carlock@gmail.com>
2011-12-12 11:45:19 -0800

    Fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Nexly::Application.routes.draw do
   end
   
   get 'features/' => 'settings#features'
-  get 'settings/' => 'settings#settings'
+  get 'settings/' => 'settings#platforms'
   get 'platforms/' => 'settings#platforms'
   get 'init_settings/' => 'settings#init_settings'
   post 'settings/toggle_feature/:id' => 'settings#toggle_feature'
