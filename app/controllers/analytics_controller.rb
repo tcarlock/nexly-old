9c2f397aedcf795e851e359d34a225ea4489f256
Tim Carlock <timothy.carlock@gmail.com>
2012-01-19 02:13:41 -0800

    Process troubleshooting

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,14 +1,18 @@
 class AnalyticsController < ApplicationController
-  def index
-      business = Business.find(params[:business_id])
-      @traffic_meta = business.traffic_meta([business.created_at, 12.months.ago].max, DateTime.current)
+	skip_before_filter :verify_authenticity_token, :only => :track_link
 
-      @total_series = @traffic_meta.get_time_series(TrafficMeta.time_series[:monthly])
-      @review_series = @traffic_meta.filter(TrafficMeta.filter_types[:link_type], PageView.page_types[:review]).get_time_series(TrafficMeta.time_series[:monthly])
-      @profile_series = @traffic_meta.filter(TrafficMeta.filter_types[:link_type], PageView.page_types[:profile]).get_time_series(TrafficMeta.time_series[:monthly])
+  def index
+    business = Business.find(params[:business_id])
+    @traffic_meta = business.traffic_meta([business.created_at, 12.months.ago].max, DateTime.current)
+
+    @total_series = @traffic_meta.get_time_series(TrafficMeta.time_series[:monthly])
+    @review_series = @traffic_meta.filter(TrafficMeta.filter_types[:link_type], PageView.page_types[:review]).get_time_series(TrafficMeta.time_series[:monthly])
+    @profile_series = @traffic_meta.filter(TrafficMeta.filter_types[:link_type], PageView.page_types[:profile]).get_time_series(TrafficMeta.time_series[:monthly])
   end
 
   def track_link
-  	TrackingLink.find_or_create_by_in_url(:in_url => params[:in_url], :out_url => params[:out_url])
+		TrackingLink.find_or_create_by_in_url(:in_url => params[:in_url], :out_url => params[:out_url])
+
+		render :status => 200
   end
 end
