0af5d87049e34657255b7acaabd7ced678281bbe
Tim Carlock <timothy.carlock@gmail.com>
2012-01-27 00:24:10 -0800

    Updated rec mailer template

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,16 +1,16 @@
 class ReviewMailer < ActionMailer::Base
   default :from => "\"Nexly Admin\" <admin@nexly.com>"
   
-  def new_review_alert review
+  def new_review_alert review, redir_url
     @review = review
+    @redir_url = redir_url
 
     mail(:to => review.business.users.first.email, :subject => "A new review has been submitted...")
   end
   
-  def new_request_alert request, redir_url
+  def new_request_alert request
     @request = request
     @sender = request.user.profile
-    @redir_url = redir_url
 
     mail(:to => request.email, :subject => "#{@sender.first_name} #{@sender.last_name} at #{request.business.name} has requested that you submit a review...")
   end
