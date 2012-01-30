7b632de5b36cea89f23072f07ff2b7a74ca3348f
Tim Carlock <timothy.carlock@gmail.com>
2012-01-30 04:54:11 -0800

    Link sharing changes and lead form testing

--- app/workers/social_worker.rb
+++ app/workers/social_worker.rb
@@ -1,9 +1,10 @@
 require 'iron_worker'
 require 'net/http'
 require 'json'
-require File.join(File.dirname(__FILE__), 'lib', 'link_helper')
 
 class SocialWorker < IronWorker::Base
+	merge '../lib/link_helper'
+	
 	include LinkHelper
 
 	attr_accessor :business_id, :message, :link
