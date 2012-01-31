10234f43cf4ea7a75b2f5cce642f15edc6d12c8f
Tim Carlock <timothy.carlock@gmail.com>
2012-01-31 12:32:35 -0800

    Temp fx for worker mixin

--- app/workers/social_worker.rb
+++ app/workers/social_worker.rb
@@ -1,10 +1,27 @@
 require 'iron_worker'
 require 'net/http'
 require 'json'
-require_relative 'link_helper'
+# require_relative 'link_helper'
 
 class SocialWorker < IronWorker::Base
-	include LinkHelper
+	# include LinkHelper
 
-	attr_accessor :business_id, :message, :link
+	attr_accessor :business_id, :message, :link, :link_tracking_url, :bitly_api_key, :shortened_url
+
+	def shorten_link url
+		# log 'Shortening URL: ' + url
+		user = 'nexly'
+	    bitly_url = URI("http://api.bitly.com/v3/shorten/?login=#{user}&apiKey=#{@bitly_api_key}&longUrl=#{CGI::escape(url)}&format=json")
+
+	    @shortened_url = JSON.parse(Net::HTTP.get(bitly_url))['data']['url']
+	    # log 'URL shortened to: ' + @shortened_url
+
+	    @shortened_url
+	end
+
+	def track_link business_id, in_url, out_url
+		# log 'Logging links to ' + @link_tracking_url + ' ' + in_url + ' | ' + out_url
+
+		Net::HTTP.post_form(URI.parse(@link_tracking_url), { :business_id => business_id, :in_url => in_url, :out_url => out_url })
+	end
 end
