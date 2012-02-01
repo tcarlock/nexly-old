de7ff098f83168a587c310e645205df4ba3c1578
Tim Carlock <timothy.carlock@gmail.com>
2012-01-31 17:59:53 -0800

    Temp fix for worker mixin

--- app/workers/social_worker.rb
+++ app/workers/social_worker.rb
@@ -2,10 +2,10 @@ require 'iron_worker'
 require 'net/http'
 require 'json'
 
-merge '../../lib/link_helper.rb'
-
 class SocialWorker < IronWorker::Base
 	include LinkHelper
+
+	merge '../../lib/link_helper.rb'
 
 	attr_accessor :business_id, :message, :link
 end
