feb0865bd9a678e8e1fcf1a2a085308d7d5e3326
Tim Carlock <timothy.carlock@gmail.com>
2012-02-05 04:29:18 -0800

    Just checking in

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -35,28 +35,36 @@ $(function(){
     	}
 	}
 
-	if($('#storyboard-banner').length == -1) {
-		$('#pre').delay(2000).fadeOut(1000, function() {
-			$('#storyboard-banner').animate({ width: 400, right: 275 });
-			$('#post').fadeIn(1500, function() {
-				$('#storyboard-banner').delay(1500).animate({ top:0, right:0, opacity: 0 }, 1000, function() {
-					$('.panel-nav-btn, #sign-up-tab, #viewport .content, #viewport .text').fadeTo(2000, 1, function() {
-						$('#viewport').cycle('resume').hover(function() {
-							if(!isHardPaused)
-								pauseAutoNav();
-						}, function() {
-							if(!isHardPaused)
-								startAutoNav();
-						});
-					});
-					$('#pre').remove();
-					$('#post').remove();
-				});
-			});
-		});
-	};
+	// if($('#storyboard-banner').length == 1) {
+	// 	$('#pre').delay(2000).fadeOut(1000, function() {
+	// 		$('#storyboard-banner').animate({ width: 400, right: 275 });
+	// 		$('#post').fadeIn(1500, function() {
+	// 			$('#storyboard-banner').delay(1500).animate({ top:0, right:0, opacity: 0 }, 1000, function() {
+	// 				$('.panel-nav-btn, #sign-up-tab, #viewport .content, #viewport .text').fadeTo(2000, 1, function() {
+	// 					$('#viewport').cycle('resume').hover(function() {
+	// 						if(!isHardPaused)
+	// 							pauseAutoNav();
+	// 					}, function() {
+	// 						if(!isHardPaused)
+	// 							startAutoNav();
+	// 					});
+	// 				});
+	// 				$('#pre').remove();
+	// 				$('#post').remove();
+	// 			});
+	// 		});
+	// 	});
+	// };
 
-	$('.panel-nav-btn, #sign-up-tab, #viewport .content, #viewport .text').fadeTo(1000, 1);
+	$('.panel-nav-btn, #sign-up-tab, #viewport .content, #viewport .text').fadeTo(1000, 1, function() {
+		$('#viewport').cycle('resume').hover(function() {
+			if(!isHardPaused)
+				pauseAutoNav();
+		}, function() {
+			if(!isHardPaused)
+				startAutoNav();
+		});
+	});
 	
 	$('#panel-play-pause').toggle(function() {
 		isHardPaused = true;
@@ -73,13 +81,13 @@ $(function(){
 
 		$('div.text, div.content', '#demo-panel').fadeOut(350, function() {
 			$('#viewport').animate({ height: '400px' }, function() {
-				$('#demo-outer').show();
+				$('#demo-container').show();
 			});
 		});
 	});
 
 	$('#close-demo').click(function() {
-		$('#demo-outer').fadeOut(250, function() {
+		$('#demo-container').fadeOut(250, function() {
 			$('#viewport').animate({ height: '340px' });
 			$('div.text, div.content', '#demo-panel').fadeIn(350, function() {
 				isHardPaused = false;
@@ -130,16 +138,16 @@ $(function(){
 		});
 	});
 	
-	$('li', '#highlights ul').hoverIntent({    
-	    over: function() {
-			var element = $(this);
-			element.find("div.content").slideDown(250);
-			// if((element.offset().top + element.height()) > $(window).height())
-			// 	$('body').animate({ scrollTop: element.offset().top }, 800);
-		},
-	    timeout:0,
-	    out: function() {
-			$(this).find("div.content").fadeOut(250);
-		}
-	});
+	// $('li', '#highlights ul').hoverIntent({    
+	//     over: function() {
+	// 		var element = $(this);
+	// 		element.find("div.content").slideDown(250);
+	// 		if((element.offset().top + element.height()) > $(window).height())
+	// 			$('body').animate({ scrollTop: element.offset().top }, 800);
+	// 	},
+	//     timeout:0,
+	//     out: function() {
+	// 		$(this).find("div.content").fadeOut(250);
+	// 	}
+	// });
 });
