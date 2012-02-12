0af1f053b08e7051f1dbdcfffd20c3c0a2476070
Tim Carlock <timothy.carlock@gmail.com>
2012-02-12 11:46:19 -0800

    Udpated data load and indexes

---
+++ db/migrate/20120212063847_add_indexes.rb
@@ -0,0 +1,13 @@
+class AddIndexes < ActiveRecord::Migration
+  def change
+  	add_index :user_profiles, :user_id
+  	add_index :reviews, :business_id
+  	add_index :review_requests, :business_id
+  	add_index :inquiries, :business_id
+  	add_index :recommendations, :business_id
+  	add_index :tracking_links, :business_id
+  	add_index :page_views, [:resource_id, :business_id, :platform_id, :resource_type_id, :channel_type_id], :name => 'index_page_views_multi_col'
+  	add_index :platform_pages, [:external_id, :business_id, :platform_id]
+  	add_index :authentications, [:business_id, :platform_id]
+  end
+end
